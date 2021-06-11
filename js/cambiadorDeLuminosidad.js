2a8c9c70800415dbd451ea40f9f5d2d487375c3b
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2021-06-10 20:22:37 -0500

    Boton de cambio de luminosidad terminado

---
+++ js/cambiadorDeLuminosidad.js
@@ -0,0 +1,42 @@
+const switchDeLuminosidad = document.querySelector('#switchDeLuminosidad');
+const iconoIndicador = document.querySelector('#iconoIndicador');
+const imagenIndicadora = document.querySelector('#imagenIconoIndicador');
+
+let coloresPorDefecto = true;
+switchDeLuminosidad.addEventListener('click', () => {
+    if(coloresPorDefecto) {
+        //Activar modo oscuro
+        iconoIndicador.style = "left: 42px;";//Desliza el switch
+        imagenIndicadora.style = 'content: url("../imgs/icons/switch/icon-luna.svg");';
+        coloresPorDefecto = false;
+        cambiarColoresDeElementos();
+    } else {
+        //Activar colores por defecto
+        iconoIndicador.style = "left: 0px;";
+        imagenIndicadora.style = 'content: url("../imgs/icons/switch/icon-sol.svg");';
+        coloresPorDefecto = true;
+        cambiarColoresDeElementos();
+    }
+});
+
+/**
+ * Cambia los colores de algunos elementos de la página.
+ * Cambia de su color por defecto a un color más oscuro y
+ * viceversa.
+ */
+function cambiarColoresDeElementos() {
+    const elementosParaModificarSuFondo = [
+        {
+            id: "#main",
+            colorPorDefecto: "white",
+            colorEnModoOscuro: "#363434"
+        }
+    ];
+
+    elementosParaModificarSuFondo.forEach((elementoDeLaLista) => {
+        let elementoAModificar = document.querySelector(elementoDeLaLista.id);
+        elementoAModificar.style = `transition: background-color 1s linear;
+            background-color: ${coloresPorDefecto ? elementoDeLaLista.colorPorDefecto: elementoDeLaLista.colorEnModoOscuro};`;
+            console.log('color cambiado');
+    });
+}
