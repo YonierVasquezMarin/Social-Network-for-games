9e653fbfbc092dc4e12a96cc1bfbe8344a45b2c2
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2021-06-13 20:25:55 -0500

    Refactorizado el script de cambiador de luminosidad

--- js/cambiadorDeLuminosidad.js
+++ js/cambiadorDeLuminosidad.js
@@ -1,33 +1,106 @@
 const switchDeLuminosidad = document.querySelector('#switchDeLuminosidad');
-const iconoIndicador = document.querySelector('#iconoIndicador');
 const imagenIndicadora = document.querySelector('#imagenIconoIndicador');
 
-let coloresPorDefecto = true;
-switchDeLuminosidad.addEventListener('click', () => {
-    if(coloresPorDefecto) {
-        //Activar modo oscuro
-        iconoIndicador.style = "left: 42px;";//Desliza el switch
+/**
+ * Retorna si el sitio tiene los estilos por defecto.
+ * @returns {Boolean} si tiene los estilos por defecto
+ * retorna true, si no (porque tiene el modo oscuro activado)
+ * retorna false.
+ */
+function estanLosEstilosPorDefecto() {
+    let estanLosEstilosPorDefecto = localStorage.getItem("estilosPorDefecto");
+
+    if(estanLosEstilosPorDefecto == null) {
+        return true;
+    } else {
+        let estado = (estanLosEstilosPorDefecto=="true");
+        if(estado) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+/**
+ * Revisa en el localStorage el si los estilos por defecto
+ * están activados. Si están activados los pone en false, de 
+ * lo contrario los pone true.
+ */
+function cambiarPreferenciaDeEstilos() {
+    if(this.estanLosEstilosPorDefecto()) {
+        localStorage.setItem("estilosPorDefecto", "false");
+    } else {
+        localStorage.setItem("estilosPorDefecto", "true");
+    }
+}
+
+/**
+ * Desliza el switch hacia la derecha o hacia la izquierda
+ * según su posición actual.
+ * @param {Boolean} deslizarHaciaLaDerecha Si esto es true el
+ * switch se desliza hacia la derecha, y si es false se desliza
+ * hacia la izquierda.
+ * @param {Boolean} deslizarConTransicion Si esto es true, el
+ * switch se desliza suavemente usando una transicion, si es
+ * false se desliza inmediatamente sin transicion.
+ */
+function deslizarElSwitch(deslizarHaciaLaDerecha, deslizarConTransicion) {
+    const iconoIndicador = document.querySelector('#iconoIndicador');
+    if (deslizarHaciaLaDerecha) {
+        if (deslizarConTransicion) {
+            iconoIndicador.style = "left: 42px;transition: left .4s ease;";
+        } else {
+            iconoIndicador.style = "left: 42px;";
+        }
         imagenIndicadora.style = 'content: url("../imgs/icons/switch/icon-luna.svg");';
-        cambiarEstilosDeElementos();
-        coloresPorDefecto = false;
+    } else {
+        if (deslizarConTransicion) {
+            iconoIndicador.style = "left: 0px;transition: left .4s ease;";
+        } else {
+            iconoIndicador.style = "left: 0px;";
+        }
+        imagenIndicadora.style = 'content: url("../imgs/icons/switch/icon-sol.svg");';
+    }
+}
+
+switchDeLuminosidad.addEventListener('click', () => {
+    let estadoDeEstilosPorDefecto = estanLosEstilosPorDefecto();
+    if(estadoDeEstilosPorDefecto) {
+        //Activar modo oscuro
+        this.deslizarElSwitch(true,true);
     } else {
         //Activar colores por defecto
-        iconoIndicador.style = "left: 0px;";
-        imagenIndicadora.style = 'content: url("../imgs/icons/switch/icon-sol.svg");';
-        cambiarEstilosDeElementos();
-        coloresPorDefecto = true;
+        this.deslizarElSwitch(false,true);
     }
+    cambiarEstilosDeElementos(estadoDeEstilosPorDefecto);
+    cambiarPreferenciaDeEstilos();
 });
 
 
 
-/**
- * Cambia los colores de algunos elementos de la página.
- * Cambia de su color por defecto a un color más oscuro y
- * viceversa.
- */
-function cambiarEstilosDeElementos() {
 
+/**
+ * Cambia los estilos de algunos elementos de la página.
+ * Cambia sus estilos por defecto a unos estilos más oscuros, y
+ * si están los estilos oscuros activados estos son cambiados
+ * a sus estilos por defecto.
+ * @param {boolean} estanLosEstilosPorDefecto Es el estado de los estilos
+ * del sitio. Si el sitio tiene sus estilos por defecto esto es
+ * true, si los estilos están en oscuros esto es false.
+ */
+function cambiarEstilosDeElementos(estanLosEstilosPorDefecto) {
+
+    /**
+     * Retorna si el objeto hace referencia a un unico elemento.
+     * @param {Object} datosDelElemento Es un objeto que contiene
+     * el id de un HTMLelement (o la clase), y los estilos del modo
+     * oscuro.
+     * @returns {Boolean} Retorna true si el objeto del parametro
+     * hace referencia a un unico elemento, y retorna false si hace
+     * referencia a multiples elementos (significa que el id de ese
+     * elemento es una clase).
+     */
     function haceReferenciaAUnUnicoElemento(datosDelElemento) {
         if(datosDelElemento.id[0]=='#') {
             return true;
@@ -50,7 +123,7 @@ function cambiarEstilosDeElementos() {
         }
     ];
 
-    if(coloresPorDefecto) {
+    if(estanLosEstilosPorDefecto) {
         //Cambiar a estilos del modo oscuro
         elementosParaModificarSusEstilos.forEach( (elementoDeLaLista) => {
             if(haceReferenciaAUnUnicoElemento(elementoDeLaLista)) {
---
+++ js/manejadorDePreferencias.js
@@ -0,0 +1,11 @@
+/* Se encarga de cargar la preferencia de luminosidad (si el modo
+    oscuro está disponible) y el ancho del menú movil. */
+
+//Nombres de las propiedades del local store:
+//-Para el fondo oscuro: estilosPorDefecto
+//-Para el ancho del menú móvil: anchoDelMenu
+
+//Elementos a modificar:
+//-fondo
+//-switch
+//-ancho del menu.
